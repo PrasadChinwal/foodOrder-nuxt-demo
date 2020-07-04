dffd6959881ff69ebb538191b00b913012cc7306
PrasadChinwal <prasadchinwal5@gmail.com>
2020-07-03 19:50:12 -0500

    Updated app name

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,7 +12,7 @@ export default {
   */
   head: {
     titleTemplate: '%s - ' + process.env.npm_package_name,
-    title: process.env.npm_package_name || '',
+    title: process.env.npm_package_name || 'Food Services',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
